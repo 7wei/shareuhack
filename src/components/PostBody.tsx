3a0ce0839dad85dfe7a93294e2db76c94be7ff07
marvincwhuang <cwhuang0523@gmail.com>
2021-07-08 01:51:20 +0800

    remove markdown style

--- src/components/PostBody.tsx
+++ src/components/PostBody.tsx
@@ -1,9 +1,7 @@
-import markdownStyles from '../styles/markdown-styles.module.css'
-
 export default function PostBody({ content }: { content: string }) {
   return (
     <div className="max-w-2xl mx-auto">
-      <div className={markdownStyles['markdown']} dangerouslySetInnerHTML={{ __html: content }} />
+      <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   )
 }
