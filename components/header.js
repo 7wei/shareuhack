bb48e5652cf2d131707348aedf27b3bbf2305620
marvincwhuang <cwhuang0523@gmail.com>
2021-07-07 22:30:54 +0800

    prettierrc

--- components/header.js
+++ components/header.js
@@ -1,12 +1,12 @@
-import {Link, Box, Typography} from '@material-ui/core'
+import { Link, Box, Typography } from '@material-ui/core'
 
 export default function Header() {
   return (
-    <Box height='140px'>
-      <Typography variant='h2'>
-      <Link href="/" color="inherit">
-        Shareuhack
-      </Link>
+    <Box height="140px">
+      <Typography variant="h2">
+        <Link href="/" color="inherit">
+          Shareuhack
+        </Link>
       </Typography>
     </Box>
   )
