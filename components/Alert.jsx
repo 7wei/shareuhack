66a59657abde7959fec2b1a57bd1be25dd69222a
marvincwhuang <cwhuang0523@gmail.com>
2021-07-07 23:24:39 +0800

    base

--- components/Alert.jsx
+++ components/Alert.jsx
@@ -1,10 +1,27 @@
-import {Box, Typography, Link} from '@material-ui/core'
+import { Link } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { Alert as MuiAlert } from '@material-ui/lab'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}))
 
 export default function Alert() {
+  const classes = useStyles()
+
   return (
-    <Box textAlign='center'>
-      <Typography align='center'>Shareuhack是仰賴用戶支持而持續產生內容的，當您透過網站內的連結購買商品，我們可能因此收益。<Link href='#'>了解更多</Link></Typography>
-    </Box>
+    <>
+      <div className={classes.root}>
+        <MuiAlert severity="info">
+          Shareuhack是仰賴用戶支持而持續產生內容的，當您透過網站內的連結購買商品，我們可能因此收益。
+          <Link href="#">了解更多</Link>
+        </MuiAlert>
+      </div>
+    </>
   )
 }
