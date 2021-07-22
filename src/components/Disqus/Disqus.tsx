24d8841c2a0486ca3b32f294ae3298ee2e025272
marvincwhuang <cwhuang0523@gmail.com>
2021-07-22 23:36:24 +0800

    use env for disqus name

--- src/components/Disqus/Disqus.tsx
+++ src/components/Disqus/Disqus.tsx
@@ -1,7 +1,7 @@
 import { DiscussionEmbed } from 'disqus-react'
 
 const Disqus = ({ title, slug }: { title: string; slug: string }) => {
-  const disqusShortname = '${process.env.NEXT_PUBLIC_DISQUS_NAME}'
+  const disqusShortname = process.env.NEXT_PUBLIC_DISQUS_NAME
   const disqusConfig = {
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/${slug}`,
     identifier: slug,
