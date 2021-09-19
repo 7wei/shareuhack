0c54252ea738f959e7a2ddc46d065bd46ff5f0d1
marvincwhuang <cwhuang0523@gmail.com>
2021-09-19 14:58:42 +0800

    add hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -33,6 +33,19 @@ export default class MyDocument extends Document {
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           ></script>
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}
+            h._hjSettings={hjid:2609307,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            }}
+          />
         </Head>
         <body>
           <Main />
