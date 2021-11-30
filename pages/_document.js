f6776783d8e5849e44d31ed16ebe27e4cc4aaf45
marvincwhuang <cwhuang0523@gmail.com>
2021-11-30 18:15:11 +0800

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -74,17 +74,6 @@ export default class MyDocument extends Document {
               })(window,document,'script','dataLayer','GTM-T32Z3BT');`,
             }}
           />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){
-              var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
-              f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
-              var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
-              _.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
-
-              var ml_account = ml('accounts', '3616085', 'z2m5d4m0k5', 'load');`,
-            }}
-          />
         </Head>
         <body>
           {/* Google Tag Manager (noscript) */}
