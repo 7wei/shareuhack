11a4b3cab661d68e73e06e64011c781f7a88042d
marvincwhuang <cwhuang0523@gmail.com>
2021-12-26 15:14:56 +0800

    disable locale detection

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'zh-TW',
     locales: ['en-US', 'en-GB', 'en-SG', 'zh-TW', 'zh-CN', 'zh-MO', 'zh-HK', 'ja-JP'],
+    localeDetection: false,
   },
   react: {
     useSuspense: false,
