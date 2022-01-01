f1b6251395a629b4fa1dfc209250e5e392da47ac
marvincwhuang <cwhuang0523@gmail.com>
2022-01-01 22:52:04 +0800

    fix

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -131,9 +131,9 @@ export const theme = {
     gradient1: '#000000 linear-gradient(180deg, rgba(255, 255, 255, 0.08) 0%, rgba(255, 255, 255, 0) 100%)',
   },
   height: {
-    header: 72,
-    footer: 120,
-    mobileHeader: 48,
+    header: '72px',
+    footer: '120px',
+    mobileHeader: '48px',
   },
   shape: {
     border: '1px solid',
