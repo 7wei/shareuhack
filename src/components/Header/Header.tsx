83d09ac4cbe97808800d043556c5292c35f9b914
marvincwhuang <cwhuang0523@gmail.com>
2022-01-07 23:49:39 +0800

    add navlink to md header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import { useCallback, useState } from 'react'
-import { Box, AppBar, Toolbar, IconButton, Container, useTheme } from '@mui/material'
+import { Box, AppBar, Toolbar, IconButton, Typography, useTheme } from '@mui/material'
 import { useRouter } from 'next/router'
 import useBreakpint from 'hooks/useBreakpoint'
 import { Menu, Close } from '@mui/icons-material'
 import dynamic from 'next/dynamic'
 import Link from 'components/Link/Link'
 import Image from 'next/image'
+import { Categories } from '../../../lib/constants'
+import { useTranslation } from 'next-i18next'
 
 const Drawer = dynamic(() => import('./Drawer'))
 
@@ -14,6 +16,7 @@ export default function Header() {
   const isDownMd = useBreakpint('md')
   const [openDrawer, setOpenDrawer] = useState(false)
   const theme = useTheme()
+  const { t } = useTranslation('common')
 
   const onClick = useCallback(() => {
     setOpenDrawer(false)
@@ -23,6 +26,9 @@ export default function Header() {
     <AppBar
       position="sticky"
       sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
         background: theme.palette.background.default,
         boxShadow: 'none',
         height: {
@@ -31,10 +37,48 @@ export default function Header() {
         },
         borderBottom: `1px solid rgba(0,0,0,0.1)`,
         overflow: 'hidden',
-        padding: '0 15px',
+        padding: {
+          xs: '0px 20px !important',
+          md: '0px 80px !important',
+        },
       }}
     >
-      <Toolbar
+      <Link href="/" locale={locale} title="Shareuhack|Home">
+        <Box
+          sx={{
+            width: {
+              xs: 100,
+              md: 144,
+            },
+            height: 48,
+            position: 'relative',
+          }}
+        >
+          <Image
+            title="shareuhack brand logo"
+            aria-label="shareuhack brand logo"
+            src={'/assets/brand/shareuhack2.svg'}
+            alt="shareuhack brand logo"
+            layout="fill"
+          />
+        </Box>
+      </Link>
+      <Box sx={{ flexGrow: 1, display: 'flex', gap: 30, ml: 48 }}>
+        {Categories.map((link) => (
+          <Link
+            key={link.key}
+            href={link.link}
+            onClick={onClick}
+            title={'Shareuhack|' + t(`categories.${link.key}.title`)}
+            disableUnderline
+          >
+            <Typography variant="body1" color={theme.palette.text.primary}>
+              {t(`categories.${link.key}.title`)}
+            </Typography>
+          </Link>
+        ))}
+      </Box>
+      {/* <Toolbar
         sx={{
           background: theme.palette.background.default,
           boxShadow: 'none',
@@ -56,31 +100,26 @@ export default function Header() {
           },
         }}
       >
-        <Link href="/" locale={locale} title="Shareuhack|Home">
-          <Box
-            sx={{
-              width: {
-                xs: 100,
-                md: 144,
-              },
-              height: 48,
-              position: 'relative',
-            }}
-          >
-            <Image
-              title="shareuhack brand logo"
-              aria-label="shareuhack brand logo"
-              src={'/assets/brand/shareuhack2.svg'}
-              alt="shareuhack brand logo"
-              layout="fill"
-            />
-          </Box>
-        </Link>
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          {Categories.map((link) => (
+            <Link
+              key={link.key}
+              href={link.link}
+              onClick={onClick}
+              color={theme.palette.primary.contrastText}
+              title={'Shareuhack|' + t(`categories.${link.key}.title`)}
+            >
+              <Typography variant="body1" color={theme.palette.text.primary}>
+                {t(`categories.${link.key}.title`)}
+              </Typography>
+            </Link>
+          ))}
+        </Box>
 
         <IconButton color="primary" aria-label="Menu" onClick={() => setOpenDrawer(!openDrawer)}>
           {openDrawer && isDownMd ? <Close fontSize="small" /> : <Menu fontSize="small" />}
         </IconButton>
-      </Toolbar>
+      </Toolbar> */}
       <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)} onClick={onClick} />
     </AppBar>
   )
