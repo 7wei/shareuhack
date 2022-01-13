3d2b430a7490a428b98b54cef993aa5a43e853a8
marvincwhuang <cwhuang0523@gmail.com>
2022-01-13 20:59:03 +0800

    update favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -71,9 +71,7 @@ export default class MyDocument extends Document {
           /> */}
           <link rel="icon" href="/favicon.svg" />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-          {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" /> */}
-          {/* <link rel="manifest" href="/favicons/site.webmanifest" /> */}
+          <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/favicons/favicon.svg" color="#000000" />
           <meta name="theme-color" content="#000000" />
         </Head>
