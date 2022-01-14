c5b597496091a53913cd2de240d00947e2333ce5
marvincwhuang <cwhuang0523@gmail.com>
2022-01-14 23:33:16 +0800

    update pixel code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,6 +38,14 @@ export default class MyDocument extends Document {
             `,
             }}
           />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=611000346658489&ev=PageView&noscript=1"
+            />
+          </noscript>
 
           <noscript
             dangerouslySetInnerHTML={{
