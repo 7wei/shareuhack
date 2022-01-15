3cf4796fa775163742d6988338f2cf4e4d772765
marvincwhuang <cwhuang0523@gmail.com>
2022-01-15 12:32:31 +0800

    update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,13 @@ import { CssBaseline, ThemeProvider as MuiThemeProvider } from '@mui/material'
 import theme from '../src/theme'
 import Layout from '../src/components/Layout/Layout'
 import { appWithTranslation } from 'next-i18next'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import * as fbq from '../lib/fpixel'
 
 export function App({ Component, pageProps }) {
+  const router = useRouter()
+
   useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side')
@@ -13,11 +18,42 @@ export function App({ Component, pageProps }) {
     }
   }, [])
 
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview()
+
+    const handleRouteChange = () => {
+      fbq.pageview()
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <React.Fragment>
       <MuiThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        {/* Global Site Code Pixel - Facebook Pixel */}
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+          }}
+        />
         <Layout>
           <Component {...pageProps} />
         </Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@mui/styles'
+import { FB_PIXEL_ID } from '../lib/fpixel'
 
 export default class MyDocument extends Document {
   render() {
@@ -22,29 +23,12 @@ export default class MyDocument extends Document {
             }}
           />
 
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '611000346658489');
-              fbq('track', 'PageView');
-            `,
-            }}
-          />
-
           <noscript>
             <img
               height="1"
               width="1"
               style={{ display: 'none' }}
-              src="https://www.facebook.com/tr?id=611000346658489&ev=PageView&noscript=1"
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
             />
           </noscript>
 
@@ -97,9 +81,9 @@ export default class MyDocument extends Document {
             }}
           /> */}
           <link rel="icon" href="/favicon.svg" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" href="/favicon.svg" />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="mask-icon" href="/favicons/favicon.svg" color="#ffffff" />
+          <link rel="mask-icon" href="/favicon.svg" color="#ffffff" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
