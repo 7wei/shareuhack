71aee14c6b81d835253a85fd454e2e3ba0089ff6
7wei <cwhuang0523@gmail.com>
2022-02-06 01:00:47 +0800

    add update time

--- pages/crypto.tsx
+++ pages/crypto.tsx
@@ -7,6 +7,7 @@ import { usePrice, usePriceSet } from '../src/hooks/usePriceSet'
 import Card from '../src/components/Card'
 import BtcLogo from '../src/assets/btc.svg'
 import { useFnG } from '../src/hooks/useFnG'
+import dayjs from 'dayjs'
 
 const LineChart = dynamic(() => import('../src/components/Chart'), {
   ssr: false,
@@ -62,7 +63,7 @@ export default function Crypto({}) {
   return (
     <>
       <Typography fontSize={36} fontWeight={700} component="h1" textAlign="center" mt={24}>
-        Crypto Dashboard
+        Crypto 儀表板
       </Typography>
       <Typography mt={12} fontSize={16} sx={{ opacity: 0.6 }} textAlign="center">
         一眼掌握加密貨幣的關鍵數據
@@ -96,10 +97,13 @@ export default function Crypto({}) {
             </Box>
 
             <Typography fontSize={24} fontWeight={700} mt={18}>
-              {FnGSeriesData.slice(-1)[0]?.value}
+              {FnGSeriesData[FnGSeriesData.length - 1]?.value}
             </Typography>
             <Typography fontSize={24} fontWeight={700} color={theme.palette.error.main}>
-              {FnGSeriesData.slice(-1)[0]?.classification}
+              {FnGSeriesData[FnGSeriesData.length - 1]?.classification}
+            </Typography>
+            <Typography fontSize={14} fontWeight={400} sx={{ opacity: 0.5 }}>
+              Last Updated at {dayjs(FnGSeriesData[FnGSeriesData.length - 1]?.time).format('YYYY-MM-DD')}
             </Typography>
           </Card>
         </Grid>
