4d260eba4a79c46b8a332586fd498a145e3e6c1f
chiwei.huang <chiwei.huang@chiweihuangdeMacBook-Air.local>
2023-12-14 01:00:29 +0800

    update mailerlite form

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -31,13 +31,11 @@ export default function Footer() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-          (function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){
-            var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
-            f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
-            var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
-            _.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
-
-            var ml_account = ml('accounts', '3616085', 'z2m5d4m0k5', 'load');
+            (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+            .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+            n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+            (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+            ml('account', '731054');
             `,
           }}
         />
@@ -51,7 +49,7 @@ export default function Footer() {
         }}
       >
         <Box width="100%" maxWidth={360} margin="0 auto">
-          <div className="ml-form-embed" data-account="3616085:z2m5d4m0k5" data-form="5224628:i3c0y2"></div>
+          <div className="ml-embedded" data-form="3HLM4x"></div>
           <Typography variant="h3" textAlign={'center'}>
             Let's keep in touch!
           </Typography>
